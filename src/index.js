8133e792f686a6ae879a2d2f2606b6a2e15ea342
integ <cergocergo41@gmail.com>
2022-08-30 12:55:27 +0300

    asar denied and todesktop req

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const fetch = require("node-fetch");
 // const overviewGraph = require("../client/src/pages/WalletPage/components/Overview/components/OverviewGraph/OverviewGraph");
 //const Web3 = require("web3")
 
-// const todesktop = require("@todesktop/runtime");
-// todesktop.init();
+const todesktop = require("@todesktop/runtime");
+todesktop.init();
 
 /*const SibApiV3Sdk = require('sib-api-v3-sdk');
 SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = 'xsmtpsib-e5a0991b4970e4f4ebe9f03f23683c08b1e45ba34e2ec48c18897cd34056fc6b-gdOaP7SNz8G5kVLR';*/
